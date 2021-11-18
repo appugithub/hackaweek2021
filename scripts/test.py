dc10c86ca873c4ebd606b8836be98140e677a643
Apparao Rongala <arao@planview.com>
2021-11-18 11:58:47 +0530

    pp-1111 testing push action 1

--- scripts/test.py
+++ scripts/test.py
@@ -4,7 +4,7 @@ print("Total arguments passed:", n)
 print("\n Sys.argv[1]", sys.argv[1])
 # Arguments passed
 print("\nName of Python script:", sys.argv[0])
- 
+print('test1') 
 print("\nArguments passed:", end = " ")
 for i in range(1, n):
     print(sys.argv[i], end = " ")
