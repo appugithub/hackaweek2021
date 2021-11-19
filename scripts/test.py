fbba30ae23c369afb1920298bf8e4a705053596a
Apparao Rongala <arao@planview.com>
2021-11-19 15:20:35 +0530

    commit 26

--- scripts/test.py
+++ scripts/test.py
@@ -1,14 +1,14 @@
 import sys
 n = len(sys.argv)
 print("Total arguments passed:", n)
-print("\n Sys.argv[1]", sys.argv[1])
-print("\n Sys.argv[2]", sys.argv[2])
+# print("\n Sys.argv[1]", sys.argv[1])
+# print("\n Sys.argv[2]", sys.argv[2])
 # Arguments passed
-print("\nName of Python script:", sys.argv[0])
-print('test1')
-print("\nArguments passed:", end = " ")
-for i in range(1, n):
-    print(sys.argv[i], end = " ")
+# print("\nName of Python script:", sys.argv[0])
+# print('test1')
+# print("\nArguments passed:", end = " ")
+# for i in range(1, n):
+#     print(sys.argv[i], end = " ")
 url = sys.argv[1]
 
 token = sys.argv[2]
