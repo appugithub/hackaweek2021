9a65755b1940e69cefad91d1a90bd9f17a325376
Appu <appu.rongala@gmail.com>
2021-11-21 22:21:10 +0530

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -57,6 +57,7 @@ for each in r.json():
    
 card_url = 'https://service.projectplace.com/api/v1/cards/15042478'
 result = requests.get(url, auth=("appu.rongala@gmail.com", token)).json()
+print(result)
 card_name = result['name']
 card_description = result['description']
 
