dbb80c17c045e6c7523e7f67fe601060b474c586
Appu <appu.rongala@gmail.com>
2021-11-22 05:18:43 +0530

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -12,75 +12,77 @@ print("Total arguments passed:", n)
 url = sys.argv[1]
 
 token = sys.argv[2]
-import requests, json
-from requests_oauthlib import OAuth1
-commits_url = "https://api.github.com/repos/appugithub/hackaweek2021/pulls/2/commits"
-r = requests.get(commits_url, auth=("appu.rongala@gmail.com", token))
-commits = [each for each in r.json()]
+context = sys.argv[3]
+print(context)
+# import requests, json
+# from requests_oauthlib import OAuth1
+# commits_url = "https://api.github.com/repos/appugithub/hackaweek2021/pulls/2/commits"
+# r = requests.get(commits_url, auth=("appu.rongala@gmail.com", token))
+# commits = [each for each in r.json()]
 
-CLIENT_KEY = '3e6ea444875da784aadae6abc5124b15'
-CLIENT_SECRET = '15d032680057485f96e074978b097179b5155d50'
-ACCESS_TOKEN_KEY = '53bf556f2b7eb9523867c8d4d6339b3a'
+# CLIENT_KEY = '3e6ea444875da784aadae6abc5124b15'
+# CLIENT_SECRET = '15d032680057485f96e074978b097179b5155d50'
+# ACCESS_TOKEN_KEY = '53bf556f2b7eb9523867c8d4d6339b3a'
 
-ACCESS_TOKEN_SECRET = 'b19e74549defb2b0fe2d4f0a397884e1bb7bc165'
+# ACCESS_TOKEN_SECRET = 'b19e74549defb2b0fe2d4f0a397884e1bb7bc165'
 
-auth_code_url = "https://api.projectplace.com/oauth2/authorize"
+# auth_code_url = "https://api.projectplace.com/oauth2/authorize"
 
-access_token = "https://api.projectplace.com/oauth2/access_token"
-
-
-test_api_url = "https://service.projectplace.com/api/v1/cards/14862291"
-
-
-def get_pp_token_from_yml():
-    oauth_pp_token = OAuth1(client_key=CLIENT_KEY, client_secret=CLIENT_SECRET,
-                            resource_owner_key=ACCESS_TOKEN_KEY,
-                            resource_owner_secret=ACCESS_TOKEN_SECRET)
-    return oauth_pp_token
-
-pp_token = get_pp_token_from_yml()
-
-#response = requests.get(url=test_api_url, auth=pp_token)
+# access_token = "https://api.projectplace.com/oauth2/access_token"
 
 
+# test_api_url = "https://service.projectplace.com/api/v1/cards/14862291"
 
 
-for each in r.json():
-   print(each['commit']['message'])
-   payload = {"attachments": [],
-           "encoded_text": each['commit']['message'],
-           "item_id": 15042478,
-           "item_name": "card",
-           "send_to_external": False,
-           "sent_from": "web"}
-   card_comment_url = 'https://service.projectplace.com/api/v3/conversations/comment'
-   requests.post(url=card_comment_url, auth=pp_token, data=payload)
+# def get_pp_token_from_yml():
+#     oauth_pp_token = OAuth1(client_key=CLIENT_KEY, client_secret=CLIENT_SECRET,
+#                             resource_owner_key=ACCESS_TOKEN_KEY,
+#                             resource_owner_secret=ACCESS_TOKEN_SECRET)
+#     return oauth_pp_token
+
+# pp_token = get_pp_token_from_yml()
+
+# #response = requests.get(url=test_api_url, auth=pp_token)
+
+
+
+
+# for each in r.json():
+#    print(each['commit']['message'])
+#    payload = {"attachments": [],
+#            "encoded_text": each['commit']['message'],
+#            "item_id": 15042478,
+#            "item_name": "card",
+#            "send_to_external": False,
+#            "sent_from": "web"}
+#    card_comment_url = 'https://service.projectplace.com/api/v3/conversations/comment'
+#    requests.post(url=card_comment_url, auth=pp_token, data=payload)
    
 
-card_url = 'https://service.projectplace.com/api/v1/cards/15042478'
-result = requests.get(url=card_url, auth=pp_token)
-r = result.json()
-print(r)
+# card_url = 'https://service.projectplace.com/api/v1/cards/15042478'
+# result = requests.get(url=card_url, auth=pp_token)
+# r = result.json()
+# print(r)
+# # print(type(r))
+# # print(result)
 # print(type(r))
-# print(result)
-print(type(r))
-card_name = r['title']
-card_description = r['description']   
-print('CARD DESCRIPTION')
-print(card_description)
+# card_name = r['title']
+# card_description = r['description']   
+# print('CARD DESCRIPTION')
+# print(card_description)
          
-print('START UPDATE PR')
-url = "https://api.github.com/repos/appugithub/hackaweek2021/pulls/2"
-payload = {
-    "title": card_name,
-    "body": card_description
-}
+# print('START UPDATE PR')
+# url = "https://api.github.com/repos/appugithub/hackaweek2021/pulls/2"
+# payload = {
+#     "title": card_name,
+#     "body": card_description
+# }
 
-r = requests.post(url, auth=("appu.rongala@gmail.com", token), json=payload)
+# r = requests.post(url, auth=("appu.rongala@gmail.com", token), json=payload)
 
-# print(r.json())
-# print('END UPDATE PR')
+# # print(r.json())
+# # print('END UPDATE PR')
 
-# print(response.__dict__)
+# # print(response.__dict__)
 
 
