06865976a874b081dfd0b3c99a1de0d3a88a5680
Appu <appu.rongala@gmail.com>
2021-11-21 22:24:21 +0530

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -56,7 +56,7 @@ for each in r.json():
    requests.post(url=card_comment_url, auth=pp_token, data=payload)
    
 card_url = 'https://service.projectplace.com/api/v1/cards/15042478'
-result = requests.get(card_url, auth=("appu.rongala@gmail.com", token)).json()
+result = requests.get(card_url, auth=("appu.rongala@gmail.com", token))
 card_name = result['name']
 card_description = result['description']
 
